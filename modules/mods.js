762d7df0ef5e6f37c797e49f0795b9bb899a6aab
ac2pic <ac2pic@gmail.com>
2018-10-11 18:06:30 -0500

    Fixed Mods has no constructor

--- modules/mods.js
+++ modules/mods.js
@@ -5,7 +5,7 @@
  * @returns {{[name: string]: ((msg: discord.Message, args: string[], command: string, console: console) => Promise}}
  */
 module.exports = function(_, util) {
-    const { Mods } = require('./mods.d/mods.js');
+    const Mods = require('./mods.d/mods.js');
     const modsInfo = new Mods();
 
     return {
